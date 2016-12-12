17bb59b69275ee80e0b947a436bf46a7c0ff6628
peterbata <peterbata@hotmail.com>
2016-12-12 10:33:40 -0500

    DEC122016

---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="header clearfix">
+        <nav>
+          <ul class="nav nav-pills pull-right">
+            <li role="presentation" class="active"><a href="#">Home</a></li>
+            <li role="presentation"><a href="#">About</a></li>
+            <li role="presentation"><a href="#">Contact</a></li>
+          </ul>
+        </nav>
+        <h3 class="text-muted">Project name</h3>
+      </div>
+</template>
+
+
+<script>
+
+</script>	
+
+
+<style>
+	https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css
+</style>
+
+
+
+
+
+
+
